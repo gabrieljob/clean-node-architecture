754c5c1d68704f8d79d7f781bc2afa60d8978ecd
Gabriel Linck <gabriel.jobc13@gmail.com>
2023-01-20 18:51:05 -0300

    feat: ensure SignUpController returns 400 if passwords not match

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -86,6 +86,23 @@ describe('SignUp controller', () => {
     )
   })
 
+  test('Should return 400 if no password confirmation failed', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'invalid_password',
+      },
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(
+      new InvalidParamError('passwordConfirmation')
+    )
+  })
+
   test('Should return 400 if an invalid email is provided', () => {
     const { sut, emailValidatorStub } = makeSut()
 
