393e78367323811d28db6b21994ee1037e28cb49
Gabriel Linck <gabriel.jobc13@gmail.com>
2023-01-21 01:29:14 -0300

    chore: add jest configs and scripts

---
+++ jest-integration-config.js
@@ -0,0 +1,5 @@
+/* eslint-disable */
+/* eslint-env es6 */
+const config = require('./jest.config')
+config.testMatch = ['**/*.test.ts']
+module.exports = config
---
+++ jest-unit-config.js
@@ -0,0 +1,5 @@
+/* eslint-disable */
+/* eslint-env es6 */
+const config = require('./jest.config')
+config.testMatch = ['**/*.spec.ts']
+module.exports = config
