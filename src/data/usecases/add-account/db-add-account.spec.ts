97c91a76bf772dc15d7f680cd069945339cfc533
Gabriel Linck <gabriel.jobc13@gmail.com>
2023-01-22 17:28:46 -0300

    test: ensure DbAddAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -122,4 +122,23 @@ describe('DBAddAccount Usecase', () => {
 
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return an account on success', async () => {
+    const { sut } = makeSut()
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@mail',
+      password: 'valid_password',
+    }
+
+    const account = await sut.add(accountData)
+
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@mail',
+      password: 'hashed_password',
+    })
+  })
 })
