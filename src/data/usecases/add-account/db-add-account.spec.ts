3a383b6876c08d64ae48917b58f47681e062fc85
Gabriel Linck <gabriel.jobc13@gmail.com>
2023-01-22 17:25:19 -0300

    test: ensure DbAddAccount throws if AddAccountRepository throws

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -103,4 +103,23 @@ describe('DBAddAccount Usecase', () => {
       password: 'hashed_password',
     })
   })
+
+  test('Should throws of AddAccountRepository throws', async () => {
+    const { addAccountRepositoryStub, sut } = makeSut()
+    jest
+      .spyOn(addAccountRepositoryStub, 'add')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      )
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@mail',
+      password: 'valid_password',
+    }
+
+    const promise = sut.add(accountData)
+
+    await expect(promise).rejects.toThrow()
+  })
 })
