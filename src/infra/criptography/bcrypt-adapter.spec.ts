2b1816566d42892d8d06e56673ee0cb1ac45409b
Gabriel Linck <gabriel.jobc13@gmail.com>
2023-01-22 21:08:50 -0300

    feat: ensure BcryptAdapter throws if bcrypt throws

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -24,4 +24,17 @@ describe('Bcrypt Adapter', () => {
 
     expect(hashedValue).toBe('any_value')
   })
+
+  test('Should throws if bcrypt throws', async () => {
+    const sut = makeSut()
+    jest
+      .spyOn(sut, 'encrypt')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      )
+
+    const promise = sut.encrypt('any_value')
+
+    await expect(promise).rejects.toThrow()
+  })
 })
