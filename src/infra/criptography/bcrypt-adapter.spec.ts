9a31a05f7f65d87c313027ed8cedd8af436dbd53
Gabriel Linck <gabriel.jobc13@gmail.com>
2023-01-22 21:03:52 -0300

    test: ensure BcryptAdapter returns a hash on success

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -9,4 +9,15 @@ describe('Bcrypt Adapter', () => {
     await sut.encrypt('any_value')
     expect(hashedSpy).toHaveBeenCalledWith('any_value', salt)
   })
+
+  test('Should return a hashed value on success', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    jest
+      .spyOn(sut, 'encrypt')
+      .mockReturnValueOnce(new Promise((resolve) => resolve('any_value')))
+    const hashedValue = await sut.encrypt('any_value')
+
+    expect(hashedValue).toBe('any_value')
+  })
 })
