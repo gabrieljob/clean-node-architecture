f64125ada40cfe3d772c9223ab56a895ee3ce387
Gabriel Linck <gabriel.jobc13@gmail.com>
2023-05-01 16:26:49 -0300

    test: ensure RequiredField validation not return if validation success

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -7,4 +7,9 @@ describe('RequiredField Validation', () => {
     const error = sut.validate({ name: 'any_name' })
     expect(error).toEqual(new MissingParamError('any_field'))
   })
+  test('Should not return if validation success', async () => {
+    const sut = new RequiredFieldsValidation('any_field')
+    const error = sut.validate({ any_field: 'any_field' })
+    expect(error).toBeFalsy()
+  })
 })
